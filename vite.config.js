53c9205debf79c4ecea82acc88355d202a874c70
Adam Lockwood <adamblockwood@gmail.com>
2025-07-12 21:34:27 -0700

    fixing tags

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/SP-Dashboard",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@tests": path.resolve(__dirname, "./tests")
+    }
+  }
 })
